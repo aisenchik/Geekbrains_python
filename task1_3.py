b1da0f60dc6ea10f30e48883bce64df31a2d2eba
aisen <isen.afanasev@gmail.com>
2021-03-05 01:11:42 +0900

    Домашнее задание № 1

---
+++ task1_3.py
@@ -0,0 +1,9 @@
+"""
+3. Узнайте у пользователя число n.
+Найдите сумму чисел n + nn + nnn. Например, пользователь ввёл число 3. Считаем 3 + 33 + 333 = 369.
+"""
+
+number = input('Введите число: ')
+result = int(number) + int(number + number) + int(number + number + number)
+
+print(f'{int(number)} + {int(number + number)} + {int(number + number + number)} = {result}')
